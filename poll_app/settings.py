31142ad0e9503a056c9b862d229c026c256f3fe2
Aman <102310628+kaurpret@users.noreply.github.com>
2022-08-30 22:17:12 +0530

    Update
    Updated

--- poll_app/settings.py
+++ poll_app/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER='mohitsingh080806@gmail.com'
-EMAIL_HOST_PASSWORD='9896075883'
+EMAIL_HOST_USER='Yourmail@gmail.com'
+EMAIL_HOST_PASSWORD='*********'
 EMAIL_USE_TLS=True
 
 
